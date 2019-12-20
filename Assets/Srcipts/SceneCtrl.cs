32833fdafa1d75faa725e4dd7fd4f277a06eca39
FeniaChris <gf.christoforou@gmail.com>
2019-12-20 18:19:41 +0200

    Initial commit

---
+++ Assets/Srcipts/SceneCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneCtrl : MonoBehaviour
+{
+
+
+    public void ChangeScene(int SceneIndex)
+
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+
+
+}
